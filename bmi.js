e42c097e718bd6cfb64966482372708eb640eafe
shreyas0b0acharya <shreyasb19386@gmail.com>
2024-05-03 22:43:36 +0530

    First release of BMI calculator

---
+++ bmi.js
@@ -0,0 +1,19 @@
+function get_result() {
+    let height = 0;
+    let weight = 0;
+    // Assuming 'p_in' is for pounds and 'k_in' is for kilograms
+    if (document.getElementById('k_in').value.length > 0) {
+      weight = parseFloat(document.getElementById('k_in').value);
+    } else if (document.getElementById('p_in').value.length > 0) {
+      weight = parseFloat(document.getElementById('p_in').value) / 2.20462;
+    } 
+    // Assuming 'i_in' is for inches and 'm_in' is for meters
+    if (document.getElementById('m_in').value.length > 0) {
+      height = parseFloat(document.getElementById('m_in').value);
+    } else if (document.getElementById('i_in').value.length > 0) {
+      height = parseFloat(document.getElementById('i_in').value) / 39.3701;
+    }
+    
+    let bmi = weight / (height * height);
+    document.getElementById('h_1').innerHTML = bmi.toFixed(2); // Rounds BMI to 2 decimal places
+  }
