9e9e6018cf394c290e45804645b19d4d400f5537
shreyas0b0acharya <168179668+shreyas0b0acharya@users.noreply.github.com>
2024-05-22 15:59:56 +0530

    Update

---
+++ memory_game.js
@@ -0,0 +1,16 @@
+let table = "<table>";
+for (let i = 0; i < 4; i++) {
+  table += "<tr>";
+  for (let j = 0; j < 4; j++) {
+    table += "<td><img src='a761744e8962b35db48f79a0e623ea1d.jpg' style='height:60px; width:60px;' id='box" + i + j + "'></td>";
+  }
+  table += "</tr>";
+}
+table += "</table>";
+document.getElementById('table').innerHTML = table;
+
+const pics = ['HD-wallpaper-lamborghini-500-hq-black-and-red-luxury~2.jpg','']
+for (let k = 0; k < 4; k++) {
+  table += "<tr>";
+  for (let l = 0; l < 4; l++) {
+    table += "<td><img sr
