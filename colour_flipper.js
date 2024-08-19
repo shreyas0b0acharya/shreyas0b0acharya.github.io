50889168136c3979195e46dcd8cac6c9160e90a2
shreyas0b0acharya <shreyasb19386@gmail.com>
2024-08-19 17:39:08 +0530

    Sorting the files by Folders

--- colour_flipper.js
+++
@@ -1,9 +0,0 @@
-// colour_flipper.js
-function colorGenerator() {
-  let green = parseInt(Math.random() * 255);
-  let blue = parseInt(Math.random() * 255);
-  let red = parseInt(Math.random() * 255);
-  
-  document.getElementById("m").style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
-  document.getElementById("n").textContent = "rgb(" + red + "," + green + "," + blue + ")";
-}
