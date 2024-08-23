adbe70ea46fe7fb388f09bd441a22e3bf8408b6d
shreyas0b0acharya <shreyasb19386@gmail.com>
2024-08-23 16:33:09 +0530

    fetch

--- workout_plan/workout-plan.js
+++ workout_plan/workout-plan.js
@@ -1,7 +1,7 @@
 function loadContent(selectedData) {
     var contentElem = document.getElementById("content_body");
-
-    fetch(`nav_html_files/${selectedData}.html`)
+    console.log(`nav_html_files/${selectedData}.txt`);
+    fetch(`nav_html_files/${selectedData}.txt`)
     .then(response => response.text())  // Corrected to response.text()
     .then(data => contentElem.innerHTML = data)  // Use innerHTML to insert the HTML content
     .catch(error => console.error("Error:", error));  // Fixed the parenthesis and added a colon after "Error"
